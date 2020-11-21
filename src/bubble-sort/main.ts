0442ae381763c194ca3425f0dbdcd19ea9ef12e2
ganesh-vellanki <velankiganesh@gmail.com>
2020-11-21 20:38:22 +0530

    feat: Add bubble sort.

--- src/bubble-sort/main.ts
+++ src/bubble-sort/main.ts
@@ -1,3 +1,19 @@
-export function bubbleSort(input_Arr: number[]): number[] {
-    return [];
+/**
+ * Bubbles sort function.
+ * @param inputArr input array.
+ * @returns sorted order.
+ */
+export function bubbleSort(inputArr: number[]): number[] {
+
+    for (let i = 0; i < inputArr.length; ++i) {
+        for (let j = i+1; j < inputArr.length; ++j) {
+            if (inputArr[i] > inputArr[j]) {
+                const temp = inputArr[i];
+                inputArr[i] = inputArr[j];
+                inputArr[j] = temp;
+            }
+        }
+    }
+
+    return inputArr;
 }
--- src/bubble-sort/test.ts
+++ src/bubble-sort/test.ts
@@ -1,5 +1,19 @@
+import { bubbleSort } from "./main";
+
 describe('bubble sort test', () => {
-    test('test 1', () => {
-        expect(1).toEqual(1);
+    test('test case #1', () => {
+        // Arrange
+        const inputArr = [2, 1];
+        console.log(inputArr);
+
+        // Action
+        const result = bubbleSort(inputArr);
+
+        // Assert
+        expect(result).not.toBeNull();
+        expect(result.length).toEqual(2);
+        expect(result[0]).toEqual(1);
+        expect(result[1]).toEqual(2);
+        console.log(inputArr);
     });
 });
